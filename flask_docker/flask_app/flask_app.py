2c233ffc747679953928e8aaaa29f59fb40caf9e
shoaib <dev@instance-1.asia-south2-a.c.development-343616.internal>
2023-07-03 04:00:12 +0000

    flask app & nginx with docker-compose

--- flask_docker/flask_app/flask_app.py
+++ flask_docker/flask_app/flask_app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello world"
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
+
+
